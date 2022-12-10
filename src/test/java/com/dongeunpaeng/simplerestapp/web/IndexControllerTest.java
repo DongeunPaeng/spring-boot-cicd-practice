70b753e633235b08a7da689bd677ec241a51849a
Dongeun Paeng <dongeun.paeng@gmail.com>
2022-12-10 21:57:56 +0900

    test: add logging message

---
+++ src/test/java/com/dongeunpaeng/simplerestapp/web/IndexControllerTest.java
@@ -0,0 +1,10 @@
+package com.dongeunpaeng.simplerestapp.web;
+
+import org.junit.jupiter.api.Test;
+
+class IndexControllerTest {
+    @Test
+    public void test() {
+        System.out.println("IndexController에 대한 테스트를 시작합니다...");
+    }
+}
