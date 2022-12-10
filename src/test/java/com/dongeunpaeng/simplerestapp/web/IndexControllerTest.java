68fdadb5195a942c7d18b53719e40d2d1d387671
Dongeun Paeng <dongeun.paeng@gmail.com>
2022-12-10 23:28:41 +0900

    test: added comment

--- src/test/java/com/dongeunpaeng/simplerestapp/web/IndexControllerTest.java
+++ src/test/java/com/dongeunpaeng/simplerestapp/web/IndexControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 class IndexControllerTest {
     @Test
     public void test() {
+        // TODO: delete log later
         System.out.println("IndexController에 대한 테스트를 시작합니다...");
     }
 }
