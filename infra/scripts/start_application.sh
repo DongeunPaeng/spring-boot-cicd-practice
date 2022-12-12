b00329674153c0a4f4b116b5a03337ea47d5c221
Dongeun Paeng <dongeun.paeng@gmail.com>
2022-12-12 10:18:41 +0900

    fix: edit start script

--- infra/scripts/start_application.sh
+++ infra/scripts/start_application.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-nohup java -jar /home/ec2-user/spring-prj/simple-rest-app-0.0.1-SNAPSHOT.jar &
+nohup java -jar /home/ec2-user/spring-prj/simple-rest-app-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
 echo "new application started"
 exit 0
