3eb31d4e2bd11e6777f4dd24469d92408e0a9786
Dongeun Paeng <dongeun.paeng@gmail.com>
2022-12-12 09:43:40 +0900

    docs: scripts 수정

--- infra/scripts/start_application.sh
+++ infra/scripts/start_application.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-nohup java -jar simple-rest-app-0.0.1-SNAPSHOT.jar &
+nohup java -jar /home/ec2-user/spring-prj/simple-rest-app-0.0.1-SNAPSHOT.jar &
 echo "new application started"
 exit 0
